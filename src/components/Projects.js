d8e6182c101c383d0b4a43e2988301e5cb9685a8
aliveevie <aliveibi080@gmail.com>
2024-02-24 14:13:29 +0100

    Update files

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,281 +1,40 @@
-import '../styles/project.css'
+import '../styles/project.css';
+
+import data from './data.js';
+
 
 export function Projects() {
-
-  
-
- 
-
-
   return (
     <div className="projects-container" id="projects">
-      <div className="project-header" > 
-            <h2
-            data-aos="zoom-out"
-            >&lt;Projects /&gt;</h2>
+      <div className="project-header">
+        <h2 data-aos="zoom-out">&lt;Projects /&gt;</h2>
       </div>
-     
 
-      <div
-        className="project-grid"
-        
-      >
-
-        {/* Project 1 */}
-        <div className="project"
-          data-aos="fade-right"
-        >
-          <h3>Wayazone Ecommerce</h3>
-          <p>
-            This is phone selling store, you can explore different types of phones here
-          </p>
-          <video src="/wayazone.webm" alt="School" 
-          controls
-          />
-          <div className="project-buttons">
-            <a href="https://www.wayazone.com/">
-              <button>Live URL</button>
-            </a>
-            <a href="https://www.wayazone.com#">
-              <button>Source Code</button>
-            </a>
+      <div className="project-grid">
+        {/* Map over project data */}
+        {data.map((project, index) => (
+          <div className="project" key={index} data-aos="fade-right">
+            <h3>{project.projectName}</h3>
+            <p>{project.description}</p>
+            <video src={project.videoSrc} alt={project.projectName} controls />
+            <div className="project-buttons">
+              <a href={project.liveUrl}>
+                <button>Live URL</button>
+              </a>
+              <a href={project.sourceCodeUrl}>
+                <button>Source Code</button>
+              </a>
+            </div>
           </div>
-        </div>
-
-
-        <div className="project"
-          data-aos="fade-right"
-        >
-          <h3>School Grading System</h3>
-          <p>
-            This is a simpe school grading system that allows teacher to easily grade and assess their students
-          </p>
-          <video src="/school.webm" alt="" 
-          controls
-          />
-          <div className="project-buttons">
-            <a href="https://school-grading.onrender.com/">
-              <button>Live URL</button>
-            </a>
-            <a href="https://github.com/aliveevie/smart-T">
-              <button>Source Code</button>
-            </a>
-          </div>
-        </div>
-
-
-        <div className="project"
-          data-aos="fade-right"
-        >
-          <h3>MedEase</h3>
-          <p>
-            This is simple medical adhrence app, that allows patient to easily track their medication adhrence
-          </p>
-          <video src="/medease.webm" alt="medease" 
-          controls
-          />
-          <div className="project-buttons">
-            <a href="https://medical-adhrence.onrender.com/">
-              <button>Live URL</button>
-            </a>
-            <a href="https://github.com/aliveevie/medication_adherence">
-              <button>Source Code</button>
-            </a>
-          </div>
-        </div>
-
-        <div className="project"
-          data-aos="fade-right"
-        >
-          <h3>Near Script</h3>
-          <p>
-            This is simple smart contract builder for developers to build on the Near ecosystem
-          </p>
-          <video src="/NearScript.webm" alt="Near Script" 
-          controls
-          />
-          <div className="project-buttons">
-            <a href="https://near-editor-app.onrender.com">
-              <button>Live URL</button>
-            </a>
-            <a href="https://github.com/aliveevie/near-editor-app">
-              <button>Source Code</button>
-            </a>
-          </div>
-        </div>
-
-        {/* Project 1 */}
-        <div className="project"
-          data-aos="fade-right"
-        >
-          <h3>Blog Post API</h3>
-          <p>
-            This is a blog post API that allows users to generate, add, edit, and delete blog posts.
-          </p>
-          <video src="/BlogPost.webm" alt="Blog Post API" 
-          controls
-          />
-          <div className="project-buttons">
-            <a href="https://blog-post-api-t5u8.onrender.com/">
-              <button>Live URL</button>
-            </a>
-            <a href="https://github.com/aliveevie/blog_posts_api">
-              <button>Source Code</button>
-            </a>
-          </div>
-        </div>
-
-        {/* Project 2 */}
-        <div className="project"
-        data-aos="fade-up"
-        >
-          <h3>Game of Life</h3>
-          <p>
-           This is basic game of life, develop with react and deploy to netlify
-          </p>
-          <video src="/gamelife.webm" alt="Game of life Project" 
-          controls
-          />
-         
-          <div className="project-buttons">
-            <a href="https://64d3bc05a4ea5b069e56b5e4--candid-lolly-c39496.netlify.app/">
-              <button>Live URL</button>
-            </a>
-            <a href="https://github.com/aliveevie/game-of-life">
-              <button>Source Code</button>
-            </a>
-          </div>
-        </div>
-         {/* Project 2 */}
-         <div className="project"
-         data-aos="fade-left"
-         >
-          <h3>Tic Tac Toe Game</h3>
-          <p>
-           This is Game of Tic tae toe!
-          </p>
-          <video src="tic-tac-game.webm" alt="Game of Tic tae" 
-          controls
-          />
-          <div className="project-buttons">
-            <a href="https://64ca878ce6fd9d17001b1211--cheery-dusk-71bebe.netlify.app/">
-              <button>Live URL</button>
-            </a>
-            <a href="https://github.com/aliveevie/Tic-toe-game">
-              <button>Source Code</button>
-            </a>
-          </div>
-        </div>
-
-        <div className="project" 
-        data-aos="fade-right"
-        >
-          <h3>Wheather Fetching API</h3>
-          <p>
-           This is an API that is Fetching Wheather
-          </p>
-          <video src="wheather.webm" alt="Wheather Fetching API image" 
-          controls
-          />
-          
-          <div className="project-buttons">
-            <a href="https://64c26d2397f23600b0ca48ad--ivione.netlify.app/">
-              <button>Live URL</button>
-            </a>
-            <a href="https://github.com/aliveevie/Local_Weathe_App">
-              <button>Source Code</button>
-            </a>
-          </div>
-        </div>
-
-        <div className="project" 
-        data-aos="fade-down"
-        >
-          <h3>Hackthon Landing Page</h3>
-          <p>
-           This is a hackathon Landing Page
-          </p>
-          <video src="landinPage.webm" alt="Landing Page video" 
-          controls
-          />
-          
-          <div className="project-buttons">
-            <a href="https://650e4ed8bef72e65833eec61--transcendent-druid-eddde8.netlify.app/">
-              <button>Live URL</button>
-            </a>
-            <a href="https://github.com/aliveevie/competition-landing-page">
-              <button>Source Code</button>
-            </a>
-          </div>
-        </div>
-
-        <div className="project" 
-        data-aos="fade-left"
-        >
-          <h3>Basic Calculator!</h3>
-          <p>
-           This is basic calculator that does most simple arithmetic
-          </p>
-          <video src="calculator.webm" alt="Smart Teaching In Schools!" 
-          controls
-          />
-          
-          <div className="project-buttons">
-            <a href="https://calculator-ecru-psi.vercel.app/">
-              <button>Live URL</button>
-            </a>
-            <a href="https://github.com/aliveevie/calculator">
-              <button>Source Code</button>
-            </a>
-          </div>
-        </div>
-
-        <div className="project" 
-        data-aos="fade-down"
-        >
-          <h3>Markdown Previewer!</h3>
-          <p>
-            This simplifies the process of creating markdown!
-          </p>
-          <video src="markdown.webm" alt="Landing Page video" 
-          controls
-          />
-          
-          <div className="project-buttons">
-            <a href="https://markdown-preview-gamma-eight.vercel.app/">
-              <button>Live URL</button>
-            </a>
-            <a href="https://github.com/aliveevie/markdown-preview">
-              <button>Source Code</button>
-            </a>
-          </div>
-        </div>
-
-        <div className="project" 
-        data-aos="fade-down"
-        >
-          <h3>Random Quote Generator</h3>
-          <p>
-           This is an app that generates code!
-          </p>
-          <video src="randomq.webm" alt="Landing Page video" 
-          controls
-          />
-          
-          <div className="project-buttons">
-            <a href="https://random-quote-gen-rosy.vercel.app/">
-              <button>Live URL</button>
-            </a>
-            <a href="https://github.com/aliveevie/Random_Quote_Gen">
-              <button>Source Code</button>
-            </a>
-          </div>
-        </div>
-
-       
-        
-
+        ))}
       </div>
     </div>
   );
 }
+
+
+
+
+
+
+ 
---
+++ src/components/data.js
@@ -0,0 +1,90 @@
+const data = [
+    {
+      projectName: "Wayazone Ecommerce",
+      description: "This is phone selling store, you can explore different types of phones here",
+      videoSrc: "/wayazone.webm",
+      liveUrl: "https://www.wayazone.com/",
+      sourceCodeUrl: "https://www.wayazone.com#"
+    },
+    {
+      projectName: "School Grading System",
+      description: "This is a simple school grading system that allows teachers to easily grade and assess their students",
+      videoSrc: "/school.webm",
+      liveUrl: "https://school-grading.onrender.com/",
+      sourceCodeUrl: "https://github.com/aliveevie/smart-T"
+    },
+    {
+      projectName: "MedEase",
+      description: "This is a simple medical adherence app, that allows patients to easily track their medication adherence",
+      videoSrc: "/medease.webm",
+      liveUrl: "https://medical-adherence.onrender.com/",
+      sourceCodeUrl: "https://github.com/aliveevie/medication_adherence"
+    },
+    {
+      projectName: "Near Script",
+      description: "This is a simple smart contract builder for developers to build on the Near ecosystem",
+      videoSrc: "/NearScript.webm",
+      liveUrl: "https://near-editor-app.onrender.com",
+      sourceCodeUrl: "https://github.com/aliveevie/near-editor-app"
+    },
+    {
+      projectName: "Blog Post API",
+      description: "This is a blog post API that allows users to generate, add, edit, and delete blog posts.",
+      videoSrc: "/BlogPost.webm",
+      liveUrl: "https://blog-post-api-t5u8.onrender.com/",
+      sourceCodeUrl: "https://github.com/aliveevie/blog_posts_api"
+    },
+    {
+      projectName: "Game of Life",
+      description: "This is a basic game of life, developed with React and deployed to Netlify",
+      videoSrc: "/gamelife.webm",
+      liveUrl: "https://64d3bc05a4ea5b069e56b5e4--candid-lolly-c39496.netlify.app/",
+      sourceCodeUrl: "https://github.com/aliveevie/game-of-life"
+    },
+    {
+      projectName: "Tic Tac Toe Game",
+      description: "This is the game of Tic Tac Toe!",
+      videoSrc: "tic-tac-game.webm",
+      liveUrl: "https://64ca878ce6fd9d17001b1211--cheery-dusk-71bebe.netlify.app/",
+      sourceCodeUrl: "https://github.com/aliveevie/Tic-toe-game"
+    },
+    {
+      projectName: "Wheather Fetching API",
+      description: "This is an API that is fetching weather",
+      videoSrc: "wheather.webm",
+      liveUrl: "https://64c26d2397f23600b0ca48ad--ivione.netlify.app/",
+      sourceCodeUrl: "https://github.com/aliveevie/Local_Weathe_App"
+    },
+    {
+      projectName: "Hackthon Landing Page",
+      description: "This is a hackathon Landing Page",
+      videoSrc: "landinPage.webm",
+      liveUrl: "https://650e4ed8bef72e65833eec61--transcendent-druid-eddde8.netlify.app/",
+      sourceCodeUrl: "https://github.com/aliveevie/competition-landing-page"
+    },
+    {
+      projectName: "Basic Calculator!",
+      description: "This is a basic calculator that does most simple arithmetic",
+      videoSrc: "calculator.webm",
+      liveUrl: "https://calculator-ecru-psi.vercel.app/",
+      sourceCodeUrl: "https://github.com/aliveevie/calculator"
+    },
+    {
+      projectName: "Markdown Previewer!",
+      description: "This simplifies the process of creating markdown!",
+      videoSrc: "markdown.webm",
+      liveUrl: "https://markdown-preview-gamma-eight.vercel.app/",
+      sourceCodeUrl: "https://github.com/aliveevie/markdown-preview"
+    },
+    {
+      projectName: "Random Quote Generator",
+      description: "This is an app that generates quotes!",
+      videoSrc: "randomq.webm",
+      liveUrl: "https://random-quote-gen-rosy.vercel.app/",
+      sourceCodeUrl: "https://github.com/aliveevie/Random_Quote_Gen"
+    }
+  ];
+
+
+  export default data;
+  
