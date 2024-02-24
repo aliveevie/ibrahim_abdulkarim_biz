5401d6352431942577b4a077092717abe5c28f16
aliveevie <aliveibi080@gmail.com>
2024-02-24 11:13:18 +0100

    Update files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Projects } from './components/Projects';
 import { Skills } from './components/Skills';
 import { Contact } from './components/Contact';
 import { Content } from './components/Content';
-import { Webapp } from './components/Webapp';
+import { Solutions } from './components/Solutions';
 import { Footer } from './components/Footer';
 import { Services } from './components/Services';
 import { Achievement } from './components/Achievement';
@@ -19,7 +19,7 @@ function App() {
         <Header/>
         <Content></Content>
         <Services/>
-        <Webapp/>
+        <Solutions />
         <Projects/>
         <Achievement />
         <Skills/>
---
+++ src/components/Solutions.js
@@ -0,0 +1,26 @@
+import "../styles/solutions.css"
+
+export function Solutions(){
+        return (
+            <>
+            <div className="eye" >
+            <div className="text" >
+
+            <img src="/ball.svg" alt="ball"
+            data-aos="fade-left"  
+            data-aos-duration="4000"
+            />
+
+            <p 
+            data-aos="fade-right"  
+            data-aos-duration="4000"
+            >I'm here to tackle your digital dilemmas, whether it's crafting decentralized apps, sparking,
+                <br></br>    startup success, or propelling your business to new heights!, 
+                <br></br></p>
+ 
+</div>
+            </div>
+         
+            </>
+        )
+}
